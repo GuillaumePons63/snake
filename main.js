a7b70dc9118952d89fdeadc63cd2b0530dcf9037
Guillaumeweb <guillaume@web-63.fr>
2022-07-08 10:59:17 +0200

    mouvment snake arrow

--- main.js
+++ main.js
@@ -1,21 +1,70 @@
 const canvas = document.getElementById('canvas');
 const button = document.querySelector('button');
 
-let dX = 10;
-let dY = 10;
 let ctx = canvas.getContext('2d');
+let blockSize = 20;
+const directionPossible = ['up', 'right', 'down', 'left'];
+let direction = directionPossible[2];
 
-function drawSnake(X, Y, longueur) {
+
+let snakes = [
+    [0, 0],
+    [0, 1],
+    [0, 2]
+]
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowUp') {
+        direction = directionPossible[0];
+    } else if (e.key === 'ArrowRight') {
+        direction = directionPossible[1];
+    } else if (e.key === 'ArrowDown') {
+        direction = directionPossible[2];
+    } else if (e.key === 'ArrowLeft') {
+        direction = directionPossible[3];
+    } else {
+        return;
+    }
+})
+
+
+function drawSnake() {
+    for (let snake of snakes) {
+        drawBlock(snake[0], snake[1]);
+    }
+    setDirection();
+
+}
+
+function setDirection() {
+    let newBlock;
+    let lastBlock = snakes[snakes.length - 1];
+    if (direction === 'up') {
+        newBlock = [lastBlock[0], lastBlock[1] - 1]
+    } else if (direction === 'right') {
+        newBlock = [lastBlock[0] + 1, lastBlock[1]]
+    } else if (direction === 'down') {
+        newBlock = [lastBlock[0], lastBlock[1] + 1]
+    } else if (direction === 'left') {
+        newBlock = [lastBlock[0] - 1, lastBlock[1]]
+    }
+    snakes.push(newBlock);
+    snakes.shift();
+}
+
+function drawBlock(x, y) {
     ctx.beginPath()
     ctx.fillStyle = 'rgb(0,215,90)';
-    ctx.fillRect(X, Y, 10, longueur, 10);
+    ctx.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);
     ctx.closePath();
+
+
 }
+
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawSnake(dX, dY, 80)
-    dY++;
+    drawSnake()
 
 }
 
