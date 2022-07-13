02a84a783331d6d49877b788cf12cb4b7d4ad948
Guillaumeweb <guillaume@web-63.fr>
2022-07-13 12:40:29 +0200

    add collision

--- main.js
+++ main.js
@@ -4,14 +4,19 @@ const button = document.querySelector('button');
 let ctx = canvas.getContext('2d');
 let blockSize = 20;
 const directionPossible = ['up', 'right', 'down', 'left'];
+
 let direction = directionPossible[2];
 
+let interval;
 
 let snakes = [
     [0, 0],
     [0, 1],
     [0, 2]
 ]
+
+
+// Gestion des touches du clavier
 
 document.addEventListener('keydown', (e) => {
     if (e.key === 'ArrowUp') {
@@ -27,13 +32,23 @@ document.addEventListener('keydown', (e) => {
     }
 })
 
+// Fonction de dessin
 
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawSnake()
+}
+function drawBlock(x, y) {
+    ctx.beginPath()
+    ctx.fillStyle = 'rgb(0,215,90)';
+    ctx.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);
+    ctx.closePath();
+}
 function drawSnake() {
     for (let snake of snakes) {
         drawBlock(snake[0], snake[1]);
     }
     setDirection();
-
 }
 
 function setDirection() {
@@ -48,27 +63,39 @@ function setDirection() {
     } else if (direction === 'left') {
         newBlock = [lastBlock[0] - 1, lastBlock[1]]
     }
+    collision(newBlock, snakes);
     snakes.push(newBlock);
     snakes.shift();
 }
 
-function drawBlock(x, y) {
-    ctx.beginPath()
-    ctx.fillStyle = 'rgb(0,215,90)';
-    ctx.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);
-    ctx.closePath();
 
+
+function collision(block, snakeArray) {
+    // Pour les colision sur le serpent en lui même
+    for (let oneBlockSnake of snakeArray) {
+        if (JSON.stringify(oneBlockSnake) === JSON.stringify(block)) {
+            gameOver();
+        }
+    }
+    // Pour empécher de sortir du canvas
+    if (block[0] < 0 || block[1] < 0 || block[0] > (canvas.width / blockSize) || block[1] > (canvas.height / blockSize)) {
+        gameOver();
+    }
 
 }
 
 
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawSnake()
 
+
+function gameOver() {
+    clearInterval(interval);
+    alert('Vous avez perdu, pfff trop nul');
+    document.location.reload();
 }
+
+
+// Lancement du jeu au clique
 
 button.addEventListener('click', () => {
-    setInterval(draw, 100)
-
+    interval = setInterval(draw, 100);
 });
